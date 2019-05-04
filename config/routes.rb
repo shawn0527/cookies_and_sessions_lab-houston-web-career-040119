dec9f58bcf03b01643ab8e1d60eb04a04e53dbe6
Shawn Ren <shawnren527@gmail.com>
2019-05-04 19:24:38 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'products#index', as: 'products'
+  get '/', to: 'products#index'
   post '/', to: 'products#add_to_cart'
 end
